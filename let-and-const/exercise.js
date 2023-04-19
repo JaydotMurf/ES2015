3b0a41deae573231caba25512e0e84bd639cc2c3
jor-el <jerraill.d.murphy@gmail.com>
2023-04-19 18:32:55 +0200

    let and const Exercise done

---
+++ let-and-const/exercise.js
@@ -0,0 +1,20 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+const PI = 3.14;
+
+// TODO: What is the difference between var and let?
+
+// ! var can be reassigned and redeclared | let can not be redeclared but can be reassigned.
+
+// TODO: What is the difference between var and const?
+
+// ! You can reassign and redeclare with var | const can not be redeclared or reassigned.
+
+// TODO: What is the difference between let and const?
+
+// !: You can reassign with let, but you can not redeclare or reassign using the const keyword.
+
+// TODO: What is hoisting?
+
+// ! mechanism where variables and function declarations are moved to the top of their scope before code execution. Inevitably, this means that no matter   where functions and variables are declared, they are moved to the top of their scope regardless of whether their scope is global or local
