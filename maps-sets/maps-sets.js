2c3f669b6b42c1e5d2c6f5a17dc89d2a992daca9
Jerraill Murphy <Jerrraill.D.Murphy@gmail.com>
2023-04-24 16:15:40 +0200

    maps-sets worksheet

---
+++ maps-sets/maps-sets.js
@@ -0,0 +1,12 @@
+// Maps and Sets
+
+// Data structures in JavaScript
+// * Data structures are formats for efficiently collecting or storing data
+// * So far we’ve seen Arrays and Objects
+// * ES2015 introduces two new ones, Maps and Sets
+
+// Maps
+// * Also called “hash maps” in other languages
+// * Until ES2015 - objects were replacements for maps
+// * Similar to objects, except the keys can be ANY data type!
+// * Created using the new keyword
